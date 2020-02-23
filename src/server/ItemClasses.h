ddd3b1e9d52c504d4a4b143260c8bd5cb0a3fbc4
bochnotomas <bochnovic@spse-po.sk>
2020-02-23 22:23:59 +0000

    just trying something

--- src/server/ItemClasses.h
+++ src/server/ItemClasses.h
@@ -1,4 +1,3 @@
-using namespace std;
 
 class Item
 {
@@ -6,12 +5,12 @@ class Item
   public:
 
   //creating the attributes
-  string itemName; //name of the item
-  string itemType; //the type of item it is i.e. weapon, armour etc
-  string itemDesc; //small description of the item the player can pull up
+  std::string itemName; //name of the item
+  std::string itemType; //the type of item it is i.e. weapon, armour etc
+  std::string itemDesc; //small description of the item the player can pull up
   char itemChar; //the character that will represent the item on the map
 
-  Item(string m_itemName, string m_itemType, string m_itemDesc, char m_itemChar) :
+  Item(std::string m_itemName, std::string m_itemType, std::string m_itemDesc, char m_itemChar) :
     itemName(m_itemName), // initializer list
     itemType(m_itemType),
     itemDesc(m_itemDesc),
@@ -22,11 +21,11 @@ class Item
 int objectCreation()
 {
   Item weaponSword("Sword", "WEAPON", "A noble blade, created by the ancient blacksmiths many years ago.", '!'); //create sword object
-  Item weaponBow("Bow", "WEAPON", "The weapon of choice for those who wish to attack from range.", "D"); //create bow object
-  Item gearVest("Armour Chestplate", "ARMOUR", "Moulded from the toughest metals found on earth, this will protect the wearer from a lot of things.", "U"); //create an armour vest object
-  Item potionSpeed("Speed potion", "POTION", "One sip of this potion will make the drinker move faster than normal.", "F"); //create speed potion object
-  Item potionHealth("Health potion", "POTION", "A little drop on the tongue will make the drinker become more resilient from enemy attacks.", "H"); //create health potion object
-  Item ammoBow("Arrow", "AMMO", "When fired, these arrows fly through the air with such finesse until they strike their target, at which point they will inflict an ungodly amount of damage.", "/"); //create arrow object
+  Item weaponBow("Bow", "WEAPON", "The weapon of choice for those who wish to attack from range.", 'D'); //create bow object
+  Item gearVest("Armour Chestplate", "ARMOUR", "Moulded from the toughest metals found on earth, this will protect the wearer from a lot of things.", 'U'); //create an armour vest object
+  Item potionSpeed("Speed potion", "POTION", "One sip of this potion will make the drinker move faster than normal.", 'F'); //create speed potion object
+  Item potionHealth("Health potion", "POTION", "A little drop on the tongue will make the drinker become more resilient from enemy attacks.", 'H'); //create health potion object
+  Item ammoBow("Arrow", "AMMO", "When fired, these arrows fly through the air with such finesse until they strike their target, at which point they will inflict an ungodly amount of damage.", '/'); //create arrow object
 
   return 0;
 }
