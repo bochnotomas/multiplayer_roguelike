fcb6e4cb9f6f1f7b3be4bf050bd24d8a9ddc292e
TheSlothinatoor <31484277+TheSlothinatoor@users.noreply.github.com>
2020-02-24 10:16:59 +0000

    Update ItemPotion.h

--- src/server/ItemPotion.h
+++ src/server/ItemPotion.h
@@ -1,6 +1,9 @@
+#ifndef ITEM_POTION_H
+#define ITEM_POTION_H
 #include "ItemClasses.h"
 
 class ItemPotion : public Item {
+    public:
     ItemPotion(string name, string type, string desc) :
         Item(name, type, desc, 'P')
     {}
@@ -8,3 +11,4 @@ class ItemPotion : public Item {
     virtual void drink() = 0; // pure virtual function
     // find the difference between a virutal function and a pure virtual
 };
+#endif
