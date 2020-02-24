4028d5c3219abbbe637378ffbe5568b7af46be97
TheSlothinatoor <31484277+TheSlothinatoor@users.noreply.github.com>
2020-02-24 10:16:10 +0000

    Update ItemArmour.h

--- src/server/ItemArmour.h
+++ src/server/ItemArmour.h
@@ -1,9 +1,13 @@
+#ifndef ITEM_ARMOUR_H
+#define ITEM_ARMOUR_H
 #include "ItemClasses.h"
 
 class ItemArmour : public Item {
+    public:
     ItemArmour(string name, string type, string desc) :
         Item(name, type, desc, 'U')
     {}
 
     virtual void extraHealth() = 0;
 };
+#endif
