4e0edefb9120751bdc799a7450f1240cecb5a163
TheSlothinatoor <31484277+TheSlothinatoor@users.noreply.github.com>
2020-02-24 10:20:13 +0000

    Update ArmourChest.h
    Removed 'U' since that's already in the class constructor

--- src/server/ArmourChest.h
+++ src/server/ArmourChest.h
@@ -6,7 +6,7 @@ using namespace std;
 
 class ArmourChest : public ItemArmour {
 	public:
-	ArmourChest() : ItemArmour("Armour Chestplate", "CHEST_ARMOUR", "Moulded from the toughest metals found on earth, this will protect the wearer from a lot of things.", 'U');
+	ArmourChest() : ItemArmour("Armour Chestplate", "CHEST_ARMOUR", "Moulded from the toughest metals found on earth, this will protect the wearer from a lot of things.");
     {
 	    
     }
